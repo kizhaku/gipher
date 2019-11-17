f83a215b40c0cc6091a2925f075b468c1a17c593
Arun Kizhakumbadan <akizhaku@in.ibm.com>
2019-11-18 03:21:16 +0530

    Compose update, manager

--- giphermanager/src/main/java/com/stackroute/giphermanager/controller/GiphBookmarkController.java
+++ giphermanager/src/main/java/com/stackroute/giphermanager/controller/GiphBookmarkController.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stackroute.giphermanager.exception.BookmarkDoesNotExistException;
@@ -22,6 +24,7 @@ import com.stackroute.giphermanager.service.MessageQueueServiceImpl;
 
 @RestController
 @RequestMapping("/giphermanager")
+@CrossOrigin(origins="*", allowedHeaders="*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.DELETE})
 public class GiphBookmarkController {
 	
 	private GiphBookmarkService giphBookmarkService;
